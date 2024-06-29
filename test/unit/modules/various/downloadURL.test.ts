c21ae5356eaf86d89aa67cbd1fda252ebacd9c21
Milos Paunovic <paun992@hotmail.com>
2024-06-29 22:41:33 +0200

    test: add another test for downloadURL function

--- test/unit/modules/various/downloadURL.test.ts
+++ test/unit/modules/various/downloadURL.test.ts
@@ -2,9 +2,15 @@ import { describe, expect, it } from 'vitest'
 import { downloadURL } from 'src/index'
 
 const INVALID = 'invalid'
+const VALID = 'https://example.com/report.pdf'
+const NAME = 'report.pdf'
 
 describe('downloadURL', () => {
   it('throws error on invalid URL', () => {
-    expect(() => downloadURL(INVALID)).toThrow('Invalid URL provided')
+    expect(() => downloadURL(INVALID, NAME)).toThrow('Invalid URL provided.')
+  })
+
+  it('throws error on missing filename', () => {
+    expect(() => downloadURL(VALID)).toThrow('Filename is required.')
   })
 })
