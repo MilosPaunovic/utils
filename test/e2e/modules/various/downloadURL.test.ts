a9cf6c42e73cd7844a7417613de954d882c32f35
Milos Paunovic <paun992@hotmail.com>
2024-06-29 19:57:02 +0200

    test: improve initial downloadURL test setup

--- test/e2e/modules/various/downloadURL.test.ts
+++ test/e2e/modules/various/downloadURL.test.ts
@@ -1,11 +1,9 @@
 import { test } from '@playwright/test'
 
-test.describe('downloadURL', () => {
-  test('downloads a file', async () => {
-    // Check that the file is downloaded successfully and exists on the filesystem
-  })
+test('downloads a file', async () => {
+  // Check that the file is downloaded successfully and exists on the filesystem
+})
 
-  test('trigers multiple downloads', async () => {
-    // Test downloadURL for multiple consecutive downloads without issues
-  })
+test('trigers multiple downloads', async () => {
+  // Test downloadURL for multiple consecutive downloads without issues
 })
