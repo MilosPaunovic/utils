63b4fc38033dfd0d3486fe4ce0453dc1aeddc6cc
Milos Paunovic <paun992@hotmail.com>
2024-06-29 20:03:19 +0200

    test: make vitest ignore e2e folder

--- test/e2e/modules/various/downloadURL.test.ts
+++ test/e2e/modules/various/downloadURL.test.ts
@@ -1,9 +1,11 @@
 import { test } from '@playwright/test'
 
-test('downloads a file', async () => {
-  // Check that the file is downloaded successfully and exists on the filesystem
-})
+test.describe('downloadURL', () => {
+  test('downloads a file', async () => {
+    // Check that the file is downloaded successfully and exists on the filesystem
+  })
 
-test('trigers multiple downloads', async () => {
-  // Test downloadURL for multiple consecutive downloads without issues
+  test('trigers multiple downloads', async () => {
+    // Test downloadURL for multiple consecutive downloads without issues
+  })
 })
